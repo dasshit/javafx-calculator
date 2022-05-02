5720e71e9018e7f0ed922c35dd8cbdcffbfec6b6
v.korobov <v.korobov@corp.mail.ru>
2022-05-02 05:22:01 +0300

    Added light/dark theme switch support

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -6,17 +6,34 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import com.jthemedetecor.*;
 
 public class CalculatorApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApplication.class.getResource("main-scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 330, 390);
+        final OsThemeDetector detector = OsThemeDetector.getDetector();
+
+        if (detector.isDark()){
+            scene.getStylesheets().add(
+                    CalculatorApplication.class.getResource("main-scene.css").toExternalForm());
+        }
 
         stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
         stage.setResizable(false);
+
+
+        detector.registerListener(isDark -> {
+            if (isDark){
+                scene.getStylesheets().add(
+                        CalculatorApplication.class.getResource("main-scene.css").toExternalForm());
+            }else{
+                scene.getStylesheets().remove(0);
+            }
+        });
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.calculator {
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
     requires java.logging;
+    requires com.jthemedetector;
 
     opens com.example.calculator to javafx.fxml;
     exports com.example.calculator;
